e8a868ddc766e4e57725530142a4feb99684460f
Weiyu Zhou <wzhou@Yelp.com>
2016-07-24 20:05:25 -0700

    change repo

--- 8weikeiOS/Pods/Target Support Files/Pods-WeikeUITests/Pods-WeikeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeikeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeikeUITests
-@end
---
+++ Weike/DiscoverViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DiscoverViewController.swift
+//  Weike
+//
+//  Created by Weiyu Zhou on 7/24/16.
+//  Copyright © 2016 Cameric. All rights reserved.
+//
+
+class DiscoverViewController: UIViewController {
+  override func loadView() {
+    self.title = "Discover"
+    self.tabBarItem = UITabBarItem(title: "Discover", image: nil, tag: 0)
+    self.view = UIView()
+  }
+
+}
