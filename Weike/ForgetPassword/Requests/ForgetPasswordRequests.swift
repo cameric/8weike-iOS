61af2819a97dd1223f08b1d60ffeef95387b62d5
Weiyu Zhou <wzhou@Yelp.com>
2016-11-05 20:08:21 -0700

    Synx

--- Weike/ForgetPassword/Requests/ForgetPasswordRequests.swift
+++ Weike/ForgetPassword/Requests/ForgetPasswordRequests.swift
@@ -8,7 +8,7 @@
 
 class ForgetPasswordRequests: APIRequest {
     static let timeout = 30.0
-//    
+//
 //    static func changePasswordTask(phone: String, password: String, completion: @escaping (_ error: Error?) -> Void) -> URLSessionDataTask? {
 //        let task = try? APIRequest.postTask("/signup/phone/mobile", params: ["phone": phone as AnyObject, "password": password as AnyObject],
 //                                            completion: { (json, error) in
@@ -16,7 +16,7 @@ class ForgetPasswordRequests: APIRequest {
 //        })
 //        return task
 //    }
-    
+
     static func verifyTask(code: String, completion: @escaping (_ error: Error?) -> Void) -> URLSessionDataTask? {
         let task = try? APIRequest.postTask("/signup/verify", params: ["code": code as AnyObject],
                                             completion: { (json, error) in
